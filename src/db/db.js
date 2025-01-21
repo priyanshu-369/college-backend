98ce1aa25767df074176feb7e2fd1541c7f8d74c
niranjan-369 <niranjan534201@gmail.com>
2025-01-21 18:17:56 +0530

    add database connection setup and environment configuration

---
+++ src/db/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+const connnectDB = async () => {
+    try {
+        // niche hum mongoose ka connection kar rahe hai database se aur iska matlab ye hai ki ye hum db se connect kar rahe hai 
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+        console.log(`\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`);
+
+    } catch (error) {
+        console.log("MongoDD connecion error: ",error);
+        process.exit(1)
+    }
+}
+
+
+export default connnectDB;
