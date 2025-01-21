fe9c80f3264fb75c0b418a040d8dc9c1a73e32fb
niranjan-369 <niranjan534201@gmail.com>
2025-01-21 19:24:08 +0530

    add CORS and cookie-parser middleware, update environment variables, and implement error handling

---
+++ src/app.js
@@ -0,0 +1,36 @@
+
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+import express from "express";
+const app = express();
+
+// hum app.use()  mostly middleware aur configuration ke liye use karte hai
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// niche hum configure kiya hai ki express sirf json ka data limit 16kb hai 
+app.use(express.json({limit: "16kb"}))
+
+app.use(express.urlencoded({extended: true, limit: "16kb" }))
+
+// ye hum koi file ko locally apne server pe store karte waqt use karenge 
+app.use(express.static("public"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export { app }
