0aaf681559b0652321c22ab35b4fae2833e1a779
niranjan-369 <niranjan534201@gmail.com>
2025-01-23 23:56:13 +0530

    feat: implement user authentication with login and logout functionality, add JWT verification middleware, and enhance user registration process

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,8 @@
 // yaha Router ko liya express se
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
-
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 //yaha router ka object banaya
 const router = Router()
@@ -10,8 +10,12 @@ const router = Router()
 // agar [ /user/registerUser ] pe req aayega to hum user register karenge...!
 router.route("/register").post(upload.single("avatar"), registerUser)
 
+// for logging in the user[owner] and admin
+router.route("/login").post(loginUser)
 
 
+// for logging in the user
+router.route("/logout").post(verifyJWT, logoutUser)
 
 
 
