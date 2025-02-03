1b78cff570d94647f8e8efe08304644643d616b9
niranjan-369 <niranjan534201@gmail.com>
2025-02-03 10:57:55 +0530

    feat: enhance staff routes with JWT verification and add appointment management endpoints

--- src/routes/staff.routes.js
+++ src/routes/staff.routes.js
@@ -1,6 +1,14 @@
 // yaha Router ko liya express se
 import { Router } from "express";
-import { loginStaff, logoutStaff } from "../controllers/staff.controller.js";
+import { verifyStaffJWT } from "../middlewares/checkRights.middleware.js";
+import { loginStaff, 
+         logoutStaff,
+         checkTotalCompletedAppointments,
+         checkTotalScheduledAppointments,
+         checkTodaysAppointments,
+         updateApointmentStatus
+ } from "../controllers/staff.controller.js";
+
 
 //yaha router ka object banaya
 const router = Router()
@@ -9,7 +17,20 @@ const router = Router()
 router.route("/login").post(loginStaff)
 
 // for logging out the user
-router.route("/logout").post(logoutStaff)
+router.route("/logout").post(verifyStaffJWT, logoutStaff)
+
+// for getting all the completed appointments by specific staff
+router.route("/appointment/completed").get(verifyStaffJWT, checkTotalCompletedAppointments)
+
+// for getting all the Scheduled appointments for specific staff
+router.route("/appointment/scheduled").get(verifyStaffJWT, checkTotalScheduledAppointments)
+
+// for getting all todays appointments for specific staff
+router.route("/appointment/today").get(verifyStaffJWT, checkTodaysAppointments)
+
+// update the status of the appointment
+router.route("/appointment/update").patch(verifyStaffJWT, updateApointmentStatus)
+
 
 
 //getting the user and staff as well as the appointment data , blogs , review if done this are on hold for some time.
